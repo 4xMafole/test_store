f7ba945b3198e2b7050b4449b822b89ae59fa56a
4xMafole <4cruxt.unknown@gmail.com>
2023-03-06 20:05:19 +0300

    feat: created a login-page.

--- lib/login/view/login_page.dart
+++ lib/login/view/login_page.dart
@@ -14,72 +14,86 @@ class _LoginPageState extends State<LoginPage> {
 
     return Scaffold(
       body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(
-              horizontal: 20, vertical: size.height * 0.1),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'eCommerce',
-                style: TextStyle(
-                    fontSize: size.width * 0.1, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: size.height * 0.05),
-              SizedBox(
-                width: size.width * 0.8,
-                child: const TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Username',
-                    border: OutlineInputBorder(),
-                  ),
+        child: SizedBox(
+          height: size.height,
+          child: Expanded(
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: size.height * 0.1,
                 ),
-              ),
-              SizedBox(height: size.height * 0.02),
-              SizedBox(
-                width: size.width * 0.8,
-                child: TextField(
-                  obscureText: _isObscure,
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    border: const OutlineInputBorder(),
-                    suffixIcon: IconButton(
-                      icon: Icon(_isObscure
-                          ? Icons.visibility_off
-                          : Icons.visibility),
-                      onPressed: () {
-                        setState(() {
-                          _isObscure = !_isObscure;
-                        });
-                      },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'eCommerce',
+                      style: TextStyle(
+                        fontSize: size.width * 0.1,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
+                    SizedBox(height: size.height * 0.05),
+                    SizedBox(
+                      width: size.width * 0.8,
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          hintText: 'User name',
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.02),
+                    SizedBox(
+                      width: size.width * 0.8,
+                      child: TextField(
+                        obscureText: _isObscure,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _isObscure
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _isObscure = !_isObscure;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.02),
+                    GestureDetector(
+                      onTap: () {
+                        // Navigate to forgot password screen
+                      },
+                      child: const Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          'Forgot Password',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.02),
+                    SizedBox(
+                      width: size.width * 0.8,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle login button press
+                        },
+                        child: const Text('Log In'),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: size.height * 0.02),
-              GestureDetector(
-                onTap: () {
-                  // Navigate to forgot password screen
-                },
-                child: const Text(
-                  'Forgot Password?',
-                  style: TextStyle(
-                    decoration: TextDecoration.underline,
-                    color: Colors.blue,
-                  ),
-                ),
-              ),
-              SizedBox(height: size.height * 0.02),
-              SizedBox(
-                width: size.width * 0.8,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Handle login button press
-                  },
-                  child: const Text('LOGIN'),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
